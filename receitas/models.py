84a384bbf11fe6023d076f658d8dba469e892540
MateusLopesNunes <mateuslopesnunes@outlook.com>
2021-11-26 23:54:20 -0300

    models

--- receitas/models.py
+++ receitas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from datetime import datetime
+from pessoas.models import Pessoa
 
 class Receita(models.Model):
 
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.CASCADE)
     nome_receita = models.CharField(max_length=200)
     ingredientes = models.TextField()
     modo_preparo = models.TextField()
@@ -10,6 +12,3 @@ class Receita(models.Model):
     rendimento = models.CharField(max_length=100)
     categoria = models.CharField(max_length=100)
     data_receita = models.DateTimeField(default=datetime.now, blank=True)
-
-    def __str__(self):
-        return self.nome_receita
